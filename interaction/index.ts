b7dfd330377323b3a182d2c34304476a382d38bf
drumath <2237k.drumath@gmail.com>
2021-07-30 16:53:37 +0900

    :sparkles: コードの追加

--- interaction/index.ts
+++ interaction/index.ts
@@ -1,17 +1,62 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import {
+  InteractionType,
+  InteractionResponseType,
+  verifyKey,
+} from "discord-interactions";
+import * as request from "request";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const CLIENT_PUBLIC_KEY = process.env.CLIENT_PUBLIC_KEY;
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const sig = req.headers["x-signature-ed25519"];
+  const time = req.headers["x-signature-timestamp"];
+  const isValid = await verifyKey(req.rawBody, sig, time, CLIENT_PUBLIC_KEY);
+
+  if (!isValid) {
+    context.res = {
+      status: 401,
+      Headers: {},
+      body: "",
+    };
+    return;
+  }
+
+  const interaction = req.body;
+  if (interaction && interaction.type === InteractionType.APPLICATION_COMMAND) {
+    const interactionId = req.body.id;
+    const interactionToken = req.body.token;
+    const greetingWord = req.body.data.options[0].value;
+    const username = req.body.member.user.username;
+
+    const url = `https://discord.com/api/v8/interactions/${interactionId}/${interactionToken}/callback`;
+
+    request.post(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `${greetingWord}, ${username}`,
+        },
+      }),
+    });
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      status: 204,
     };
-
+  } else {
+    context.res = {
+      body: JSON.stringify({
+        type: InteractionResponseType.PONG,
+      }),
+    };
+  }
 };
 
 export default httpTrigger;
